627e58a2d5dd65aa860e79c9b456d45a17cdf533
jejejeiwiw072-collab <jejejeiwiw072@gmail.com>
2026-05-02 14:29:55 +0700

    Update bug apa si ngonten

--- vinder.py
+++ vinder.py
@@ -889,4 +889,4 @@ def fast_mp3_api():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=port, threaded=True) 
